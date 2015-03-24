e5596a1056ea47477389bd00ed8c043eed62650e
gretchenziegler <gretchenziegler@gmail.com>
2015-03-24 14:38:16 -0400

    started refactoring with award methods

--- award.rb
+++ award.rb
@@ -1,1 +1,60 @@
-Award = Struct.new(:name, :expires_in, :quality)
+class Award
+
+	def initialize(name, expires_in, quality)
+		@name = name
+		@expires_in = expires_in
+		@quality = quality
+	end
+
+	attr_accessor :name, :expires_in, :quality
+
+	def normal_award
+		@expires_in -= 1
+		if @quality > 0
+			@quality -= 1
+		end
+		if @quality > 0 && @expires_in <= 0
+			@quality -=1
+		end
+	end
+
+	def blue_distinction_plus_award
+		@quality = 80
+	end
+
+	def blue_first_award
+		@expires_in -= 1
+		if @expires_in <= 0 && @quality < 49
+			@quality += 2
+		elsif @quality < 50
+			@quality += 1
+		end
+	end
+
+	def blue_compare_award
+		@expires_in -= 1
+    if @expires_in < 0
+      @quality = 0
+    elsif @quality < 48 && @expires_in < 5
+      @quality += 3
+    elsif @quality < 49 && @expires_in < 10
+      @quality += 2
+    elsif @quality < 50
+      @quality += 1
+    end
+	end
+
+	def blue_star_award
+		@expires_in -= 1
+    if @expires_in <= 0 && @quality > 3
+      @quality -= 4
+    elsif @quality > 1
+      @quality -= 2
+    elsif @quality == 1
+      @quality -= 1
+    else
+      @quality = 0
+    end
+	end
+
+end
--- update_quality.rb
+++ update_quality.rb
@@ -1,66 +1,25 @@
 require './award'
 
 def update_quality(awards)
+
   awards.each do |award|
-
-    # decrease days to expiration by one for all awards, no matter the type
-    award.expires_in -= 1
-
     case award.name
-      # BF awards increase in quality the older they get, but cannot have a quality greater than 50
-      when "Blue First"
-        if award.quality < 49 && award.expires_in <= 0
-          award.quality += 2
-        elsif award.quality < 50
-          award.quality += 1
-        end
-
-      # BDP awards never decrease in quality; they are always at 80
-      # According to the tests, they also never expire
-      when "Blue Distinction Plus"
-        award.expires_in += 1
-        award.quality = 80
-
-      # BC awards increase as the expiration date approaches; +2 at 10 or fewer days, +3 at 5 or fewer days, 0 quality after it has expired; quality cannot get above 50
-      when "Blue Compare"
-        if award.expires_in < 0
-          award.quality = 0
-        elsif award.quality < 48 && award.expires_in < 5
-          award.quality += 3
-        elsif award.quality < 49 && award.expires_in < 10
-          award.quality += 2
-        elsif award.quality < 50
-          award.quality += 1
-        end
-
-      # BS awards lose quality twice as fast as any other award
-      when "Blue Star"
-        if award.expires_in <= 0 && award.quality > 3
-          award.quality -= 4
-        elsif award.quality > 1
-          award.quality -= 2
-        elsif award.quality == 1
-          award.quality -= 1
-        else
-          award.quality = 0
-        end
-
-      # other awards decrease by one point a day until they have expired (and then they degrade twice as fast, but don't go below 0) 
-      else
-        if award.expires_in > 0 && award.quality > 0
-          award.quality -= 1
-        else
-          if award.quality > 1
-            award.quality -= 2
-          elsif award.quality == 1
-            award.quality -= 1
-          else
-            award.quality = 0
-          end
-        end
-        
+    when "NORMAL ITEM"
+      award.normal_award
+    when "Blue First"
+      award.blue_first_award
+    when "Blue Compare"
+      award.blue_compare_award
+    when "Blue Distinction Plus"
+      award.blue_distinction_plus_award
+    when "Blue Star"
+      award.blue_star_award
     end
-  end
+  end 
+
 end
+
+
+
 
 
